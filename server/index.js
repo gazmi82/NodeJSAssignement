7a52ecbe8849dd6cdd1394c4c4452c21fec1970e
gazmir sulcaj <external@emadema.com>
2021-03-23 17:33:30 +0100

    update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const init = async () => {
     port: 3000,
     host: "localhost",
   });
-
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "<h3>Hiiiiiiii!</h3>";
+    },
+  });
   await server.start();
   console.log("Server running");
 };
