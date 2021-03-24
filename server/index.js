77f6de8fe3262fda3e661fd9044f7118b3e402f9
gazmir <gazmir.sulcaj@epitech.eu>
2021-03-24 18:26:28 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+const clientId = "8166c53a1352db263e15";
+const clientSecret = "a6279f02c7e1b5a3ce291ee90fefa1c2a509d24e";
 
 const init = async () => {
   const server = Hapi.server({
@@ -15,8 +25,13 @@ const init = async () => {
     },
   });
   await server.start();
-  console.log("Server running");
+  console.log("Server running on port 5000");
 };
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connected");
+});
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
